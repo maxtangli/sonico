9a999aba736e0add6637bfe209d287848cbdbc22
tang <maxtangli@qq.com>
2017-04-30 14:36:52 +0000

    try flask templates.

--- python/flask/main.py
+++ python/flask/main.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 def index():
     return 'Index Page'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+#@app.route('/hello')
+#def hello():
+#    return 'Hello, World'
 
 @app.route('/user/<username>')
 def show_user_profile(username):
@@ -18,9 +18,28 @@ def show_user_profile(username):
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
+
+@app.route('/projects/')
+def projects():
+    # allow projects, projects
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    # allow about, not allow /about
+    return 'The about page'
 """
 day2 finish tutorial, try pip,venv,flask 60min
 thoughts
 - python> import this # the zen of python
 - modern toolchains, microframework...python vs ruby?
+"""
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+"""
+day4 try flask templates 20min
+thoughts
+- too many time cost in setup env issues, there should have a better way to leanr.
 """
---
+++ python/flask/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+. venv/bin/activate
+export FLASK_APP=main.py
+export FLASK_DEBUG=1
+flask run
