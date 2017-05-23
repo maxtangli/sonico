dc402b27f28f4bc10c8ecd7c811227f11bb493c7
tang <maxtangli@qq.com>
2017-05-23 21:59:54 +0900

    php: session.

--- php/session.php
+++ php/session.php
@@ -1,7 +1,18 @@
 <?php
 
 /**
- * security set
+ * session hijacking attack:
+ *  attacker may stole user's session id.
+ * security strategy:
+ * - use https.
+ * - expire earlier.
+ */
+
+/**
+ * session fixation attack:
+ *  attack provide a url with uninitialized session id. e.g. http://example.org/test.php?PHPSESSID=1234
+ *  user who accessed the url will use a session id chosen by attacker.
+ * security strategy:
  * - session.use_only_cookies=1: to reject param transferred session ID.
  * - session.use_strict_mode=1: to reject uninitialized session ID.
  */
@@ -25,9 +36,9 @@ if (empty($_SESSION['count'])) {
 } else {
     $_SESSION['count']++;
 }
-echo session_id(), "\n", $_SESSION['count'], "\n";
+echo session_name(), "\n", session_id(), "\n", $_SESSION['count'], "\n";
 
-// todo time-stamp based session management
+// todo: know how frameworks handle session security
 
 /**
  * serialize $_SESSION, pass it to session.save_handler
