27cd768d462a82dfba2a673f845d9b3c7eaed93d
tang <maxtangli@qq.com>
2017-06-07 22:41:31 +0900

    python: teabreak.

---
+++ python/teabreak/teabreak.py
@@ -0,0 +1,16 @@
+def range_check(func):
+    # return lambda m, e, n, c: ((0 <= m and m < n) and func(m, e, n, c)) or ""
+    return lambda m, e, n, c: ((0 <= m < n) and func(m, e, n, c)) or ""
+
+
+@range_check
+def f(m, e, n, c):
+    if pow(m, e) % n == c:
+        return str(m)
+    return ""
+
+
+if __name__ == "__main__":
+    for i in range(1000000):
+        if f(i, 17, 3569, 915) == str(i):
+            print(i)
