e7743c785b38082fedb3e20400db5338f0fe4e5c
tang <maxtangli@qq.com>
2017-06-08 22:42:17 +0900

    python: Bars, morse_decode.

--- python/teabreak/second_code.py
+++ python/teabreak/second_code.py
@@ -32,3 +32,41 @@ class SimpleBars(list):
 
     def __str__(self):
         return "".join(self)
+
+
+class Bars(SimpleBars):
+    def write(self, pos, ch):
+        n = len(self)
+        i = (pos + n) % n
+        # [ ] + i -> i, [ ] + T -> T, i + i -> [ ], else error
+        maps = [" ii", " TT", " II", "ii ", "TiI", "iTI", "IiT", "iIT"]
+        for s in maps:
+            if self[i] == s[0] and ch == s[1]:
+                self[i] = s[2]
+                return
+        raise Exception()
+
+    def next(self):
+        # i -> iTi, T -> i, i + i -> []
+        result = Bars(" " * len(self))
+        for i in range(len(self)):
+            if self[i] == "i":
+                result.write(i - 1, "i")
+                result.write(i, "T")
+                result.write(i + 1, "i")
+            elif self[i] == "I":
+                result.write(i - 1, "i")
+                result.write(i, "I")
+                result.write(i + 1, "i")
+            elif self[i] == "T":
+                result.write(i, "i")
+            else:
+                pass
+
+        for i in range(len(result)):
+            self[i] = result[i]
+
+        return self
+
+    def __str__(self):
+        return "".join(self)
---
+++ python/teabreak/third_code.py
@@ -0,0 +1,30 @@
+def decode_morse_single(s):
+    keys = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+    values = [
+        "iI", "Iiii", "IiIi", "Iii", "i", "iiIi", "IIi",
+        "iiii", "ii", "iIII", "Iii", "iIii", "II", "Ii",
+        "III", "iIIi", "IIiI", "iIi", "iii", "I",
+        "iiI", "iiiI", "iII", "IiiI", "IiII", "IIii",
+        "iIIII", "iiIII", "iiiII", "iiiiI", "iiiii", "Iiiii", "IIiii", "IIIii", "IIIIi", "IIIII"
+    ]
+    pos = values.index(s)
+    return keys[pos]
+
+
+def decode_morse(s):
+    result = ""
+    token = ""
+    for ch in s:
+        if ch != " ":
+            token += ch
+        else:
+            if token != "":
+                result += decode_morse_single(token)
+                result += " "
+                token = ""
+            else:
+                result += " "
+    return result
+
+    # print(decode_morse_single("iIIII"))
+    # print(decode_morse("iIIII iIIII  iIIII   iIIII"))
