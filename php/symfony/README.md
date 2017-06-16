cb5cfed47806fd500aee1d29bb5a52fde6d0861e
tang <maxtangli@qq.com>
2017-06-16 22:18:17 +0900

    php: read symfony asset.

---
+++ php/symfony/README.md
@@ -0,0 +1,3 @@
+# symfony source reading
+
+0. asset 0.5h // dedicated abstractions, browser game use case.
---
+++ php/symfony/asset.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\Asset\PathPackage;
+use Symfony\Component\Asset\UrlPackage;
+use Symfony\Component\Asset\VersionStrategy\StaticVersionStrategy;
+
+/**
+ * path    -> Package: getVersion($path), getUrl($path)
+ * version -> Strategy: getVersion($path), applyVersion($path)
+ */
+
+$versionStrategy = new StaticVersionStrategy('v1', '%s?v=%s');
+$defaultPackage = new Package($versionStrategy);
+$namedPackages = [
+    'img' => new UrlPackage('http://img.example.com/', $versionStrategy),
+    'doc' => new PathPackage('/somewhere/deep/for/documents', $versionStrategy),
+];
+$packages = new Packages($defaultPackage, $namedPackages);
+
+echo $packages->getUrl('/main.css') . PHP_EOL;
+// result: /main.css?v1
+
+echo $packages->getUrl('/logo.png', 'img') . PHP_EOL;
+// result: http://img.example.com/logo.png?v1
+
+echo $packages->getUrl('/resume.pdf', 'doc') . PHP_EOL;
+// result: /somewhere/deep/for/documents/resume.pdf?v1
